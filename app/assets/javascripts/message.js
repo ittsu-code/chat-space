fc569b60fa043206765e4ddad865bb72b3fcac57
ittsu <ittsu.nkmt@gmail.com>
2019-10-13 01:09:23 +0900

    messages#createの呼び出し

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,5 +2,14 @@ $(function () {
   $('#new_message').on('submit', function (e) {
     e.preventDefault();
     var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "post",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false,
+    })
   })
 })
