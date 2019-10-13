036d493cdc071443e7e784158b4e913de987ec93
ittsu <ittsu.nkmt@gmail.com>
2019-10-13 19:23:49 +0900

    テキストフィールドの整形

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   def index
     User.where('name LIKE(?)', "%#{params[:keyword]}%")
+    respond_to do |format|
+        format.html
+        format.json
+    end
   end
 end
