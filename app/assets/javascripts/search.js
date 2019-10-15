787b1b6c11d7f6e47b02a5cf1b67a5a975069c71
ittsu <ittsu.nkmt@gmail.com>
2019-10-15 16:48:55 +0900

    インクリメンタルサーチ実装

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,28 +1,36 @@
 $(function () {
 
+  var search_list = $("#search-result");
+
   function appendUser(user) {
     var html = `
     <div class="chat-group-user clearfix">
-      <p class="chat-group-user__name">${user.name}</p>
-    <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</div>
-    </div>
+  <p class="chat-group-user__name">${user.name}</p>
+  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+</div>
     `
+    search_list.append(html);
   }
 
+  function appendErrMsgToHTML(msg) {
+    var html = `<div class='.chat-group-user clearfix'>${msg}</div>`
+    search_list.append(html);
+  }
 
   $("#user-search-field").on("keyup", function () {
     var input = $("#user-search-field").val();
+    console.log(input)
 
     $.ajax({
-      type: 'GET',
-      url: '/users/index',
+      type: 'get',
+      url: '/users',
       data: { keyword: input },
       dataType: 'json'
     })
 
       .done(function (users) {
-        $("検索結果").empty();
-        if (user.length !== 0) {
+        $("#search-result").empty();
+        if (users.length !== 0) {
           users.forEach(function (user) {
             appendUser(user);
           });
