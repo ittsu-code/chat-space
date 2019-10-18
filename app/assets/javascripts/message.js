5c2ab883b8e8597bc82382fc30540a6474aede41
ittsu <ittsu.nkmt@gmail.com>
2019-10-18 21:59:58 +0900

    カスタムデータ属性反映

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -5,7 +5,7 @@ $(function() {
         ? `<img class="lower-message__image" src=${message.image}>`
         : "";
     var html = `
-    <div class="message">
+    <div class="message" data-id=${message.id}>
       <div class="message__upper-info">
         <div class="message__upper-info__talker">
         ${message.user}
@@ -58,72 +58,5 @@ $(function() {
       .fail(function() {
         console.log("error");
       });
-  };
-  var buildMessageHTML = function(message) {
-    if (message.content && message.image.url) {
-      //data-idが反映されるようにしている
-      var html =
-        '<div class="message" data-id=' +
-        message.id +
-        ">" +
-        '<div class="upper-message">' +
-        '<div class="upper-message__user-name">' +
-        message.user_name +
-        "</div>" +
-        '<div class="upper-message__date">' +
-        message.created_at +
-        "</div>" +
-        "</div>" +
-        '<div class="lower-message">' +
-        '<p class="lower-message__content">' +
-        message.content +
-        "</p>" +
-        '<img src="' +
-        message.image.url +
-        '" class="lower-message__image" >' +
-        "</div>" +
-        "</div>";
-    } else if (message.content) {
-      //同様に、data-idが反映されるようにしている
-      var html =
-        '<div class="message" data-id=' +
-        message.id +
-        ">" +
-        '<div class="upper-message">' +
-        '<div class="upper-message__user-name">' +
-        message.user_name +
-        "</div>" +
-        '<div class="upper-message__date">' +
-        message.created_at +
-        "</div>" +
-        "</div>" +
-        '<div class="lower-message">' +
-        '<p class="lower-message__content">' +
-        message.content +
-        "</p>" +
-        "</div>" +
-        "</div>";
-    } else if (message.image.url) {
-      //同様に、data-idが反映されるようにしている
-      var html =
-        '<div class="message" data-id=' +
-        message.id +
-        ">" +
-        '<div class="upper-message">' +
-        '<div class="upper-message__user-name">' +
-        message.user_name +
-        "</div>" +
-        '<div class="upper-message__date">' +
-        message.created_at +
-        "</div>" +
-        "</div>" +
-        '<div class="lower-message">' +
-        '<img src="' +
-        message.image.url +
-        '" class="lower-message__image" >' +
-        "</div>" +
-        "</div>";
-    }
-    return html;
   };
 });
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,4 +1,4 @@
-json.(@message, :text :image)
+json.(@message, :text, :image)
 json.created_at @message.created_at
 json.user @message.user.name
 json.id @message.id
