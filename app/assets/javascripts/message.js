2ba320e772a6977b7398fa1d3e92f2baa297b08e
ittsu <ittsu.nkmt@gmail.com>
2019-10-18 18:18:31 +0900

    アクションのリクエスト

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,9 @@
-$(function () {
+$(function() {
   function buildHTML(message) {
-    var image = (message.image !== null) ? `<img class="lower-message__image" src=${message.image}>` : ""
+    var image =
+      message.image !== null
+        ? `<img class="lower-message__image" src=${message.image}>`
+        : "";
     var html = `
     <div class="message">
       <div class="message__upper-info">
@@ -14,30 +17,46 @@ $(function () {
     <div class="message__text"></div>
     ${message.text}
     ${image}
-    </div>`
+    </div>`;
     return html;
   }
-  $('#new_message').on('submit', function (e) {
+  $("#new_message").on("submit", function(e) {
     e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr('action')
+    var url = $(this).attr("action");
     $.ajax({
       url: url,
       type: "post",
       data: formData,
-      dataType: 'json',
+      dataType: "json",
       processData: false,
-      contentType: false,
+      contentType: false
     })
-      .done(function (data) {
+      .done(function(data) {
         var html = buildHTML(data);
-        $('.messages').append(html)
-        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight });
-        $('.new_message')[0].reset();
-        $('.submit-btn').attr('disabled', false);
+        $(".messages").append(html);
+        $(".messages").animate({ scrollTop: $(".messages")[0].scrollHeight });
+        $(".new_message")[0].reset();
+        $(".submit-btn").attr("disabled", false);
       })
-      .fail(function () {
-        alert('error');
+      .fail(function() {
+        alert("error");
+      });
+  });
+
+  var reloadMessage = function() {
+    var last_message_id = $(".message:last").data("message-id");
+    $.ajax({
+      url: "/api/messages",
+      type: "get",
+      dataType: "json",
+      data: { id: last_message_id }
+    })
+      .done(function(messages) {
+        console.log("success");
       })
-  })
-})
+      .fail(function() {
+        console.log("error");
+      });
+  };
+});
