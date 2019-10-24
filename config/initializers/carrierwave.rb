45d982a3a4ab5eefbcd2824dae404d9e52c2afb9
ittsu <ittsu.nkmt@gmail.com>
2019-10-25 05:22:00 +0900

    修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'chatspaceinkmt'
-  config.asset_host = 'https://s3-  ap-northeast-1.amazonaws.com/chatspaceinkmt'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chatspaceinkmt'
 end
