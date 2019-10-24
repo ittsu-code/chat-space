58c5b88262cedd7a5baf8bab1137fa6960597dc9
ittsu <ittsu.nkmt@gmail.com>
2019-10-25 06:06:25 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -46,12 +46,12 @@ $(function () {
 
   var reloadMessages = function () {
     if (window.location.href.match(/\/groups\/\d+\/messages/)) {
-      var last_message_id = $(".message:last").data("message-id");
+      var last_message_id = $(".message:last").data("id");
       $.ajax({
         url: "api/messages",
         type: "get",
         dataType: "json",
-        data: { last_id: last_message_id }
+        data: { id: last_message_id }
       })
         .done(function (messages) {
           var insertHTML = "";
