e39abb9a54cf94f8589eb46d7220ead334791cb1
ittsu <ittsu.nkmt@gmail.com>
2019-10-25 18:06:44 +0900

    rollback

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -45,31 +45,27 @@ $(function () {
   });
 
   var reloadMessages = function () {
-    if (window.location.href.match(/\/groups\/\d+\/messages/)) {
-      var last_message_id = $('.message:last').attr('id');
-      $.ajax({
-        url: "api/messages",
-        type: "get",
-        dataType: "json",
-        data: { id: last_message_id },
-        processData: false,
-        contentType: false
-      })
-        .done(function (messages) {
-          var insertHTML = "";
-          messages.forEach(function (message) {
-            insertHTML = buildHTML(message);
-            $(".messages").append(insertHTML);
-            $(".messages").animate(
-              { scrollTop: $(".messages")[0].scrollHeight },
-              "fast"
-            );
-          });
-        })
-        .fail(function () {
-          console.log("error");
+    var last_message_id = $(".message:last").data("id");
+    $.ajax({
+      url: "api/messages",
+      type: "get",
+      dataType: "json",
+      data: { id: last_message_id }
+    })
+      .done(function (messages) {
+        var insertHTML = "";
+        messages.forEach(function (message) {
+          insertHTML = buildHTML(message);
+          $(".messages").append(insertHTML);
+          $(".messages").animate(
+            { scrollTop: $(".messages")[0].scrollHeight },
+            "fast"
+          );
         });
-    };
-  }
+      })
+      .fail(function () {
+        console.log("error");
+      });
+  };
   setInterval(reloadMessages, 5000);
 });
